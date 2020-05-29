db98ac4bcd539cd5fac6c4d7210c58b7c24f9432
Frederik Dietz <fdietz@gmail.com>
2020-05-29 09:45:31 +0200

    devise and bootstrap setup

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  # use this instead of email_changed? for Rails = 5.1.x
+  def will_save_change_to_email?
+    false
+  end
+end
